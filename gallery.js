47b403fb0b785569e6d0adcb4cb6f3e1e6ffee2b
Auxile-Isaac <auxile.isaac@gmail.com>
2024-06-06 14:48:36 +0200

    I changed the way I was doing things. more javascript

---
+++ gallery.js
@@ -0,0 +1,134 @@
+const galleryContainer = document.getElementById("gallery-container");
+const fullSizeAnimalImageContainer = document.querySelector('.full-size-image');
+
+const animals = [
+  {
+    id: 1,
+    name: "FOX",
+    species: "FENNEC",
+    origin: "India",
+    image: "./images/fox.jpeg",
+  },
+  {
+    id: 2,
+    name: "WHALE",
+    species: "HUMPBACK",
+    origin: "South Africa",
+    image: "./images/whale.jpeg",
+  },
+  {
+    id: 3,
+    name: "BABOON",
+    species: "COMMON BROWN",
+    origin: "South Africa",
+    image: "./images/baboon.jpeg",
+  },
+  {
+    id: 4,
+    name: "DEER",
+    species: "SPOTTED",
+    origin: "Amazon",
+    image: "./images/deer.jpeg",
+  },
+];
+
+const animalContainerFragment = document.createDocumentFragment();
+
+const displayGalleryAnimalInfo = (animalInfo) => {
+  const animalInfoContainer = document.createElement("li");
+  animalInfoContainer.classList = "image-info";
+  animalInfoContainer.dataset.id = animalInfo.id;
+
+  const animalImgContainer = document.createElement("div");
+  animalImgContainer.classList = " animal-image";
+
+  const animalImage = document.createElement("img");
+  animalImage.src = animalInfo.image;
+  animalImage.alt = animalInfo.name;
+
+  animalImgContainer.appendChild(animalImage);
+
+  const animalName = document.createElement("p");
+  animalName.className = "animal-name";
+  animalName.textContent = animalInfo.name;
+
+  const animalSpecies = document.createElement("p");
+  animalSpecies.classList = "animal-species";
+  animalSpecies.textContent = animalInfo.species;
+
+  const animalLocation = document.createElement("p");
+  animalLocation.classList = "animal-location";
+  animalLocation.textContent = animalInfo.origin;
+
+  const knowMoreButton = document.createElement("button");
+  knowMoreButton.classList = "know-more-btn";
+
+  const btnParagaraph = document.createElement("p");
+  btnParagaraph.textContent = "Know more";
+
+  const btnIcon = document.createElement("i");
+  btnIcon.classList = "ri-arrow-right-line";
+
+  knowMoreButton.append(btnParagaraph, btnIcon);
+
+  const animalDetails = document.createElement("div");
+  animalDetails.classList = "animal-details";
+
+  animalDetails.append(
+    animalSpecies,
+    animalName,
+    animalLocation,
+    knowMoreButton
+  );
+
+  animalInfoContainer.append(animalImgContainer, animalDetails);
+
+  animalContainerFragment.appendChild(animalInfoContainer);
+};
+
+animals.forEach((animal) => {
+  displayGalleryAnimalInfo(animal);
+});
+
+
+
+const showAnimalInFullScreen = (animal) => {
+    const fullSizeFragment = document.createDocumentFragment();
+
+    const closeButton = document.createElement('i');
+    closeButton.classList.add('ri-close-circle-fill', 'close-full-size-view');
+    closeButton.id = 'close-image-view';
+
+    const animalImage = document.createElement("img");
+    animalImage.src = animal.image;
+    animalImage.alt = animal.name;
+
+    fullSizeFragment.append(closeButton, animalImage);
+    
+    fullSizeAnimalImageContainer.innerHTML = '';
+
+    fullSizeAnimalImageContainer.appendChild(fullSizeFragment);
+    fullSizeAnimalImageContainer.classList.add('full-size-image-view');
+}
+
+const hideFullScreen = () => {
+    fullSizeAnimalImageContainer.classList.remove('full-size-image-view');
+}
+
+galleryContainer.addEventListener('click', (e) => {
+    const animalCard = e.target.closest('.image-info');
+  if (animalCard) {
+    const animalId = parseInt(animalCard.dataset.id);
+    const animal = animals.find(animal => animal.id === animalId);
+    showAnimalInFullScreen(animal);
+  }
+  });
+
+  fullSizeAnimalImageContainer.addEventListener('click', (e) => {
+    if (e.target.matches('.close-full-size-view')) {
+      hideFullScreen();
+      fullSizeAnimalImageContainer.innerHTML = '';
+    }
+  });
+
+  galleryContainer.appendChild(animalContainerFragment);
